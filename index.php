820e5f0fa211b5090a54683821ef65cd5d6b6b52
Frode Aabak <frode_aabak@hotmail.com>
2022-11-22 17:46:17 +0100

    Created index.php file with some content.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Github repository test by Frode Aabak</title>
+    <style>
+        a {color:#0080c0}
+        a:hover {color:#00a0e0}
+        body {font-family:'Segoe UI','Helvetica';font-size:15px}
+        h1,h2,h3,h4,h5,h6 {font-weight:normal}
+    </style>
+</head>
+<body>
+    <p align="center">
+    <h1>Simply a test</h1>
+    Just a simple test to see if things appear in Github.<br><br>
+    ..seems like it did!
+    </p>
+</body>
+</html>
